c95021224c01a00cd53f687c899fc126d2f2ee3a
Ashparty <bug73@AsharooGaming>
2020-11-26 20:29:35 -0700

    Added Darker Forest + armour change

--- src/main/java/net/mcreator/thenine/item/PekoiumArmorItem.java
+++ src/main/java/net/mcreator/thenine/item/PekoiumArmorItem.java
@@ -36,7 +36,7 @@ public class PekoiumArmorItem extends TheNineModElements.ModElement {
 	public void initElements() {
 		IArmorMaterial armormaterial = new IArmorMaterial() {
 			public int getDurability(EquipmentSlotType slot) {
-				return new int[]{13, 15, 16, 11}[slot.getIndex()] * 26;
+				return new int[]{13, 15, 16, 11}[slot.getIndex()] * 27;
 			}
 
 			public int getDamageReductionAmount(EquipmentSlotType slot) {
