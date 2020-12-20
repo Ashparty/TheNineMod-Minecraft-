f683271c4eb93b095773ae0f6936fbecf40a25e1
Ashparty <Ashpartymc@gmail.com>
2020-12-20 15:19:29 -0700

    Added Slowing and Quickness Enchantment Placeholders

---
+++ src/main/java/net/mcreator/thenine/enchantment/SlowingEnchantment.java
@@ -0,0 +1,54 @@
+
+package net.mcreator.thenine.enchantment;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.enchantment.Enchantment;
+
+import net.mcreator.thenine.TheNineModElements;
+
+@TheNineModElements.ModElement.Tag
+public class SlowingEnchantment extends TheNineModElements.ModElement {
+	@ObjectHolder("the_nine:slowing")
+	public static final Enchantment enchantment = null;
+	public SlowingEnchantment(TheNineModElements instance) {
+		super(instance, 367);
+	}
+
+	@Override
+	public void initElements() {
+		elements.enchantments.add(() -> new CustomEnchantment(EquipmentSlotType.MAINHAND).setRegistryName("slowing"));
+	}
+	public static class CustomEnchantment extends Enchantment {
+		public CustomEnchantment(EquipmentSlotType... slots) {
+			super(Enchantment.Rarity.RARE, EnchantmentType.WEAPON, slots);
+		}
+
+		@Override
+		public int getMinLevel() {
+			return 1;
+		}
+
+		@Override
+		public int getMaxLevel() {
+			return 2;
+		}
+
+		@Override
+		public boolean isTreasureEnchantment() {
+			return false;
+		}
+
+		@Override
+		public boolean isCurse() {
+			return true;
+		}
+
+		@Override
+		public boolean isAllowedOnBooks() {
+			return true;
+		}
+	}
+}
