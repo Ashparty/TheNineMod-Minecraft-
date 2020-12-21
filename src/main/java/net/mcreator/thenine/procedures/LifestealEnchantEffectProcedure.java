2c95440d3533b30952b3f9cee029d65cf5e0a2d1
Ashparty <Ashpartymc@gmail.com>
2020-12-21 11:24:58 -0700

    Added Life Steal Enchantment

---
+++ src/main/java/net/mcreator/thenine/procedures/LifestealEnchantEffectProcedure.java
@@ -0,0 +1,86 @@
+package net.mcreator.thenine.procedures;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.enchantment.EnchantmentHelper;
+
+import net.mcreator.thenine.enchantment.LifeStealEnchantment;
+import net.mcreator.thenine.TheNineModElements;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@TheNineModElements.ModElement.Tag
+public class LifestealEnchantEffectProcedure extends TheNineModElements.ModElement {
+	public LifestealEnchantEffectProcedure(TheNineModElements instance) {
+		super(instance, 379);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure LifestealEnchantEffect!");
+			return;
+		}
+		if (dependencies.get("sourceentity") == null) {
+			if (!dependencies.containsKey("sourceentity"))
+				System.err.println("Failed to load dependency sourceentity for procedure LifestealEnchantEffect!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		Entity sourceentity = (Entity) dependencies.get("sourceentity");
+		if (((((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)).isEnchanted())) {
+			if (((EnchantmentHelper.getEnchantmentLevel(LifeStealEnchantment.enchantment,
+					((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY))) == 1)) {
+				if (sourceentity instanceof LivingEntity)
+					((LivingEntity) sourceentity).addPotionEffect(new EffectInstance(Effects.REGENERATION, (int) 20, (int) 1, (false), (false)));
+			} else if (((EnchantmentHelper.getEnchantmentLevel(LifeStealEnchantment.enchantment,
+					((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY))) == 2)) {
+				if (sourceentity instanceof LivingEntity)
+					((LivingEntity) sourceentity).addPotionEffect(new EffectInstance(Effects.REGENERATION, (int) 20, (int) 2, (false), (false)));
+			} else if (((EnchantmentHelper.getEnchantmentLevel(LifeStealEnchantment.enchantment,
+					((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY))) == 3)) {
+				if (sourceentity instanceof LivingEntity)
+					((LivingEntity) sourceentity).addPotionEffect(new EffectInstance(Effects.REGENERATION, (int) 20, (int) 3, (false), (false)));
+			} else if (((EnchantmentHelper.getEnchantmentLevel(LifeStealEnchantment.enchantment,
+					((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY))) == 4)) {
+				if (entity instanceof LivingEntity)
+					((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.REGENERATION, (int) 20, (int) 4, (false), (false)));
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onEntityAttacked(LivingAttackEvent event) {
+		if (event != null && event.getEntity() != null) {
+			Entity entity = event.getEntity();
+			Entity sourceentity = event.getSource().getTrueSource();
+			Entity imediatesourceentity = event.getSource().getImmediateSource();
+			double i = entity.getPosX();
+			double j = entity.getPosY();
+			double k = entity.getPosZ();
+			double amount = event.getAmount();
+			World world = entity.world;
+			Map<String, Object> dependencies = new HashMap<>();
+			dependencies.put("x", i);
+			dependencies.put("y", j);
+			dependencies.put("z", k);
+			dependencies.put("amount", amount);
+			dependencies.put("world", world);
+			dependencies.put("entity", entity);
+			dependencies.put("sourceentity", sourceentity);
+			dependencies.put("imediatesourceentity", imediatesourceentity);
+			dependencies.put("event", event);
+			this.executeProcedure(dependencies);
+		}
+	}
+}
