8eabc1ac7c585f36dc9ddcde54496f27d2507bd1
Ashparty <Ashpartymc@gmail.com>
2020-12-21 08:47:41 -0700

    Added Quickness And Slowness Enchantments + Effects of enchants

---
+++ src/main/java/net/mcreator/thenine/procedures/SlowingEnchantEffectProcedure.java
@@ -0,0 +1,66 @@
+package net.mcreator.thenine.procedures;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraft.potion.Effects;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.enchantment.EnchantmentHelper;
+
+import net.mcreator.thenine.enchantment.SlowingEnchantment;
+import net.mcreator.thenine.TheNineModElements;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@TheNineModElements.ModElement.Tag
+public class SlowingEnchantEffectProcedure extends TheNineModElements.ModElement {
+	public SlowingEnchantEffectProcedure(TheNineModElements instance) {
+		super(instance, 376);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure SlowingEnchantEffect!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		if (((((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY)).isEnchanted())) {
+			if (((EnchantmentHelper.getEnchantmentLevel(SlowingEnchantment.enchantment,
+					((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY))) == 1)) {
+				if (entity instanceof LivingEntity)
+					((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SLOWNESS, (int) 20, (int) 1, (false), (false)));
+			} else if (((EnchantmentHelper.getEnchantmentLevel(SlowingEnchantment.enchantment,
+					((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY))) == 2)) {
+				if (entity instanceof LivingEntity)
+					((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SLOWNESS, (int) 20, (int) 2, (false), (false)));
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		if (event.phase == TickEvent.Phase.END) {
+			Entity entity = event.player;
+			World world = entity.world;
+			double i = entity.getPosX();
+			double j = entity.getPosY();
+			double k = entity.getPosZ();
+			Map<String, Object> dependencies = new HashMap<>();
+			dependencies.put("x", i);
+			dependencies.put("y", j);
+			dependencies.put("z", k);
+			dependencies.put("world", world);
+			dependencies.put("entity", entity);
+			dependencies.put("event", event);
+			this.executeProcedure(dependencies);
+		}
+	}
+}
